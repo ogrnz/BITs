226f69d06d4099007617cfc6b47d0c4f31a866c8
Potrac <olivier.grognuz@gmail.com>
2020-02-13 21:52:43 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Script to scrape content of mapped BITs (Bilateral Investment Treaties):
+https://investmentpolicy.unctad.org/international-investment-agreements/iia-mapping
+
+The data of the upper link is formatted as a python list in the cleaned_rows.py file. This list was last updated on 13/02/2020 (dd/mm/yy). Please be aware that new treaties are regularly added.
+
+The mapped content of each treaty is then scraped using script1.py. All data are then converted into a json file: data.json.
---
+++ script1.py
@@ -0,0 +1,131 @@
+from bs4 import BeautifulSoup
+import requests
+import json
+import time
+import sys
+
+# strip contents of '\n'
+def cleanN(el, parent):
+    if isinstance(el, str):
+        parent.contents.remove(el)
+    return
+
+
+t1 = time.time()
+
+urls = []
+with open('urls.txt', 'r') as f:
+    for line in f:
+        urls.append(line.strip())
+
+urls = ['https://investmentpolicy.unctad.org/international-investment-agreements/treaties/bit/64/algeria---italy-bit-1991-']
+treaties = []
+
+sys.exit()
+for i, url in enumerate(urls):
+    i += 1
+
+    print(f'Retrieving informations: {i}/{len(urls)}')
+
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, 'html.parser')
+
+    # Informations per treaty   
+    informations = {}
+
+    # First elements
+    firstEls = soup.find_all('p', class_="form-control-static")
+    descs = ['name', 'Treaty type', 'Status', 'Date of signature', 'Date of entry into force', 'Treaty full text']
+    informations[descs[0]] = soup.find('div', class_='page-title').h2.text
+
+    for el, desc in zip(firstEls[0:4], descs[2:6]):
+        informations[desc] = el.text.strip()
+
+    if firstEls[4].a is None:
+        informations['Treaty full text'] = 'https://investmentpolicy.unctad.org' + firstEls[3].a['href']
+    else:
+        informations['Treaty full text'] = 'https://investmentpolicy.unctad.org' + firstEls[4].a['href']
+
+    # Informations per criteria
+    informations['criteria'] = {}
+
+    # Get panel headings "div.panel-heading", create dict for each section
+    headings = soup.find_all('div', class_='panel-heading')
+
+    for heading in headings:
+        # Create criteria dict
+        informations['criteria'][heading.text.strip()] = {}
+        BASE = informations['criteria'][heading.text.strip()]
+
+        # Get next sibling of each panel heading, those are sections 
+        section = heading.next_sibling.next_sibling
+        
+        # Inside each section, get each div.row
+        rows = section.find_all('div', class_='row')
+
+        lastCritLvl1 = None
+        lastCritLvl2 = None
+
+        # Loop through each row
+        for row in rows:   
+            # Clean list of \n       
+            for el in row.contents:
+                cleanN(el, row)
+            
+            # if only 1 child, create new dict, continue 
+            if len(row.contents) == 1:
+                if lastCritLvl1 is None:
+                    BASE[row.contents[0].text.strip()] = {}
+                    lastCritLvl1 = row.contents[0].text
+                else:   
+                    BASE[lastCritLvl1][row.contents[0].text.strip()] = {}  
+                    lastCritLvl2 = row.contents[0].text
+            
+            # if there is 2 child div, key: value
+            else:
+                if lastCritLvl1 is None:
+                    BASE[row.contents[0].text.strip()] = row.contents[1].text.strip()
+                else:
+                    nextElement = row.contents[0].parent.next_sibling.next_sibling
+
+                    if lastCritLvl2 is None: 
+                        BASE[lastCritLvl1][row.contents[0].text.strip()] = row.contents[1].text.strip()
+
+                        if nextElement is not None:
+                            # strip contents of '\n'
+                            for el in nextElement:
+                                cleanN(el, nextElement)
+
+                            # it's a criteria        
+                            if len(nextElement.contents) == 1:
+                                if len(nextElement.contents[0]['class']) == 1:
+                                    lastCritLvl1 = None
+                    else:
+                        BASE[lastCritLvl1][lastCritLvl2][row.contents[0].text.strip()] = row.contents[1].text.strip()
+
+                        if nextElement is not None:
+                            # strip contents of '\n'
+                            for el in nextElement:
+                                if isinstance(el, str):
+                                    cleanN(el, nextElement)
+
+                            # it's a criteria        
+                            if len(nextElement.contents) == 1:
+                                if len(nextElement.contents[0]['class']) == 1:
+                                    lastCritLvl1 = None
+                                    lastCritLvl2 = None
+                                if len(nextElement.contents[0]['class']) == 2:
+                                    lastCritLvl2 = None
+    # Append to treaties dict                                
+    treaties.append(informations)
+    time.sleep(0.5)
+
+elapsed = time.time() - t1
+
+print('Finished retrieving informations in', elapsed, 's')
+print('Writing to data.json')
+
+with open('data.json', 'w') as outfile:
+    json.dump(treaties, outfile)
+
+print('Done.')
