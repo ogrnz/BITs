c860cf9e19ecb722ea8ecad4e53b3a25710006b8
Potrac <olivier.grognuz@gmail.com>
2020-03-30 23:25:38 +0300

    added rows-generator.py

--- README.md
+++ README.md
@@ -3,6 +3,11 @@
 Script to scrape content of mapped BITs (Bilateral Investment Treaties):
 https://investmentpolicy.unctad.org/international-investment-agreements/iia-mapping
 
-The data of the upper link is formatted as a python list in the cleaned_rows.py file. This list was last updated on 13/02/2020 (dd/mm/yy). Please be aware that new treaties are regularly added.
+The data of the upper link is formatted as a python list in the `cleaned_rows.py` file. This list was last updated on 13/02/2020 (dd/mm/yy). Please be aware that new treaties are regularly added, so you should scrape it again to have the latest data.
 
-The mapped content of each treaty is then scraped using script1.py. All data are then converted into a json file: data.json.
+The mapped content of each treaty is then scraped using `script1.py`. All data are then converted into a file named data.json.
+
+If you wish to use the data in a `<html>` table, feel free to use the `rows-generator.py` script to generate html code.
+
+## TODO
+Gotta format data.json into a csv file to be usable on R and such.
---
+++ rows-generator.py
@@ -0,0 +1,23 @@
+# Use the rows variable from cleaned_rows.py
+rows = [['URL', 'Short title', 'Type', 'Status', 'Parties', 'Date of signature', 'Date of entry into force', 'Termination date', 'Text URL'],['/international-investment-agreements/treaties/bit/1/afghanistan---germany-bit-2005-', 'Afghanistan - Germany BIT (2005)', 'BITs', 'In force', 'Afghanistan, Germany', '20/04/2005', '12/10/2007', '', 'Full text: en']]
+base = 'https://investmentpolicy.unctad.org'
+
+def format_rows(rows):
+    table = ''
+    for i, row in enumerate(rows[1:], start=1):
+        table += f"""<tr>
+        <td class="id">{ i }</td>
+        <td class="url"><a href="{ base + row[0] }">url</a></td>
+        <td class="short-title">{ row[1] }</td>
+        <td class="type">{ row[2] }</td>
+        <td class="status">{ row[3] }</td>
+        <td class="parties">{ row[4] }</td>
+        <td class="signature">{ row[5] }</td>
+        <td class="entry">{ row[6] }</td>
+        <td class="termination">{ row[7] }</td>
+        </tr>\n"""
+    return table
+
+with open('rows.html', 'w') as f:
+    f.write(format_rows(rows))
+print('Done.')
