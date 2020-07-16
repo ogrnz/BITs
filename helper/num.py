1f4c5bf7000ec141e665b7b159f402fff8e9ad89
Potrac <olivier.grognuz@gmail.com>
2020-07-16 12:52:03 +0200

    Rearrange data, party1 & 2 extracted

--- helper/num.py
+++ helper/num.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import re
 
 treaties = pd.read_csv('./data/data.csv')
 
@@ -13,5 +14,37 @@ del treaties['Treaty investment chapter text']
 # Strip 'Parties' column of \r\n
 treaties['Parties'] = treaties['Parties'].replace('\\r\\n', ' ', regex = True) 
 
-# Export df into new csv
+def get_parties(parties):
+    party = re.split(r'\d', parties)
+
+    # First item of list is empty, delete it
+    del party[0]
+
+    # Some cleaning
+    party = [p.replace('.', '') for p in party]
+    party = [p.strip(' ') for p in party]
+    
+    # If there is one party missing, let's catch the error
+    try:
+        party1 = party[0]
+        party2 = party[1]
+    except IndexError:
+        party2 = 'ERROR'
+    
+    return party1, party2
+
+treaties['Party1'] = treaties['Parties'].map(lambda x: get_parties(x)[0])
+treaties['Party2'] = treaties['Parties'].map(lambda x: get_parties(x)[1])
+
+# Check if it seems ok
+print(treaties['Party1'].describe())
+print(treaties['Party2'].describe())
+
+# Rearrange column names
+cols = ['Party1', 'Party2'] + [col for col in treaties if col != 'Party1' and col != 'Party2']
+treaties = treaties[cols]
+
+print(treaties.describe())
+
+# Export dataframe into new csv
 treaties.to_csv('./data/data_cleaned.csv')
