d2e03c2f30e46d7965d4e77addf0ec86fcfb867b
Potrac <olivier.grognuz@gmail.com>
2020-07-21 23:19:17 +0200

    Added criteria

--- helper/1-replace.py
+++ helper/1-replace.py
@@ -24,7 +24,8 @@ special_cols = [
             "criteria/Investor-State Dispute Settlement (ISDS)/Forums/ISDS forum options/UNCITRAL",
             "criteria/Investor-State Dispute Settlement (ISDS)/Forums/ISDS forum options/Other forums",
             "criteria/Investor-State Dispute Settlement (ISDS)/Other specific ISDS features/Provisional measures",
-            "criteria/Investor-State Dispute Settlement (ISDS)/Other specific ISDS features/Consolidation of claims"
+            "criteria/Investor-State Dispute Settlement (ISDS)/Other specific ISDS features/Consolidation of claims",
+            "criteria.Preamble.Reference.to.social.investment.aspects..e.g..human.rights..labour..health..CSR..poverty.reduction"
             ]
 data_columns = list(df.columns)
 
@@ -32,6 +33,43 @@ data_columns = list(df.columns)
 for col in data_columns:
     if col in special_cols:
         df[col].replace(to_replace = ['Yes', 'No'], value = [-1, 0], inplace=True)
+    # cols with special replacement values
+    elif col == 'criteria/Scope and Definitions/Temporal scope of the treaty/Investments covered':
+        df[col].replace(to_replace = ['Applies to post-BIT investments only', "Applies to both pre-existing and post-BIT investments",'Not stipulated'], value = [1, -1, 0], inplace=True)
+    elif col == 'criteria/Scope and Definitions/Temporal scope of the treaty/Disputes covered':
+        df[col].replace(to_replace = ['Carves out pre-existing disputes', 'Not stipulated'], value = [1, 0], inplace=True)
+    elif col == 'criteria/Standards of Treatment/National treatment (NT)/Type of NT clause':
+        df[col].replace(to_replace = ['Post-establishment', 'Pre-establishment only', 'Pre- and post-establishment', 'None'], value = [1, 1, -1, 0], inplace=True)
+    elif col == 'criteria/Standards of Treatment/Most-favoured-nation (MFN) treatment/Type of MFN clause':
+        df[col].replace(to_replace = ['Post-establishment', 'Pre-establishment only', 'Pre- and post-establishment', 'None'], value = [1, 1, -1, 0], inplace=True)
+    elif col == 'criteria/Standards of Treatment/Fair and equitable treatment (FET)/Type of FET clause':
+        df[col].replace(to_replace = ['FET unqualified', 'FET qualified', 'None'], value = [1, -1, 0], inplace=True)
+    elif col == 'criteria/Standards of Treatment/Fair and equitable treatment (FET)/FET qualified/By reference to international law':
+        df[col].replace(to_replace = ['International law / principles of international law', 'Customary international law (CIL)', 'None'], value = [1, 1, 0], inplace=True)
+    elif col == 'criteria/Standards of Treatment/Full protection and security':
+        df[col].replace(to_replace = ['With reference to domestic law', 'Standard', 'No clause'], value = [1, -1, 0], inplace=True)
+    elif col == 'criteria/Standards of Treatment/Expropriation/Scope of measures covered':
+        df[col].replace(to_replace = ['Indirect expropriation not mentioned', 'Indirect expropriation mentioned', 'No expropriation clause'], value = [0, -1, 1], inplace=True)
+    elif col == 'criteria/Standards of Treatment/Protection from strife/Specifications/Relative right to compensation (comparator)':
+        df[col].replace(to_replace = ['MFN only', 'NT only', 'MFN and NT', 'None'], value = [-1, -1, -1, 0], inplace=True)
+    elif col == 'criteria/Standards of Treatment/Prohibition of performance requirements (PRs)/Includes prohibition of PRs':
+        df[col].replace(to_replace = ['Explicit PR clause', 'No explicit PR clause'], value = [-1, 0], inplace=True)
+    elif col == 'criteria/Standards of Treatment/Prohibition of performance requirements (PRs)/Type of PR clause':
+        df[col].replace(to_replace = ['TRIMS reference', 'List of prohibited PRs'], value = [-1, -1], inplace=True)
+    elif col == 'criteria/Exceptions/Scheduling and reservations (in treaty texts and annexes)':
+        df[col].replace(to_replace = ['Positive-list commitments', 'Negative-list reservations', 'Both positive list commitments and negative list reservations', 'None'], value = [1, 1, 1, 0], inplace=True)
+    elif col == 'criteria/Investor-State Dispute Settlement (ISDS)/Alternatives to arbitration':
+        df[col].replace(to_replace = ['Voluntary ADR (conciliation / mediation)', 'Compulsory ADR (conciliation / mediation)', 'None'], value = [0, 1, 0], inplace=True)
+    elif col == 'criteria/Investor-State Dispute Settlement (ISDS)/Scope and consent/Scope of claims: general approach (chapeau paragraph of ISDS clause)':
+        df[col].replace(to_replace = ['Covers any dispute relating to investment', 'Lists specific bases of claim beyond treaty (e.g. contractual disputes)', 'Covers treaty claims only', 'Other'], value = [-1, -1, -1, 0], inplace=True)
+    elif col == 'criteria/Investor-State Dispute Settlement (ISDS)/Forums/Relationship between forums':
+        df[col].replace(to_replace = ['No reference', 'Fork in the road', 'No U turn (waiver clause)', 'Preserving right to arbitration after domestic court proceedings', 'Local remedies first'], value = [0, 1, 1, -1, 1], inplace=True)
+    elif col == 'criteria/Treaty Duration, Amendment and Termination/Treaty duration/Years of initial treaty term':
+        df[col].replace(to_replace = ['5 years', '10 years', '15 years', '20 years', 'Other'], value = [1, -1, -1, -1, 0], inplace=True)
+    elif col == 'criteria/Treaty Duration, Amendment and Termination/Automatic renewal':
+        df[col].replace(to_replace = ['None', 'Indefinite term', '2 years', '5 years', '10 years', '15 years', '20 years', 'Other'], value = [0, -1, 1, 1, -1, -1, -1, 0], inplace=True)
+    elif col == 'criteria/Treaty Duration, Amendment and Termination/Amendment and termination/""Survival""/""sunset"" clause length':
+        df[col].replace(to_replace = ['None', '5 years', '10 years', '15 years', '20 years', 'Other'], value = [0, 1, 1, -1, -1, 0], inplace=True)
     else:
         df[col].replace(to_replace = ['Yes', 'No'], value = [1, 0], inplace=True)
 
